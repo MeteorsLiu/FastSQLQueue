6aa7dd5fc90dded33677e565fe3e97e41e7e5763
Meteor <coopticks@outlook.com>
2021-12-15 23:07:28 +0800

    Add files via upload

--- mysqltranslate.go
+++ mysqltranslate.go
@@ -1,31 +1,29 @@
 package mysqlqueue
 
 import (
+	"errors"
 	"fmt"
 	"strings"
-	"errors"
 )
 
-
-
-
-
 func BindParam(SQL, Type string, args ...interface{}) (string, error) {
-	TypeLength := len(Type)-1 
-	ArgsLength := len(args)-1
-	if (ArgsLength != TypeLength) {
+	TypeLength := len(Type) - 1
+	ArgsLength := len(args) - 1
+	if ArgsLength != TypeLength {
 		return "", errors.New("Out of indexs")
 	}
 	var sb strings.Builder
 	flag := 0
 	for _, v := range SQL {
-		if flag > TypeLength  {
-			return "", errors.New("Out of indexs")
-		}
 		if v == '?' {
-			sb.WriteString("'%")
-			sb.WriteByte(Type[flag])
-			sb.WriteString("'")
+			if flag <= TypeLength {
+				sb.WriteString("'%")
+				sb.WriteByte(Type[flag])
+				sb.WriteString("'")
+
+			} else {
+				return "", errors.New("Out of indexs")
+			}
 			flag++
 		} else {
 			sb.WriteRune(v)
@@ -35,6 +33,6 @@ func BindParam(SQL, Type string, args ...interface{}) (string, error) {
 		args[i] = Mysql_real_escape_string(v)
 	}
 
-	return fmt.Sprint(sb.String(), args...), nil
+	return fmt.Sprintf(sb.String(), args...), nil
 
 }

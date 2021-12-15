74520d8b1e8632cbcef213a13531bef7980388cc
Meteor <coopticks@outlook.com>
2021-12-15 22:13:59 +0800

    Add files via upload

---
+++ mysqltranslate.go
@@ -0,0 +1,37 @@
+package mysqlqueue
+
+import (
+	"fmt"
+	"strings"
+)
+
+
+
+
+
+func FormatSQLAndEscape(SQL, Type string, args ...interface{}) {
+	TypeLength := len(Type)
+	var sb strings.Builder
+	flag := 0
+	for _, v := range SQL {
+		if flag > TypeLength {
+			break
+		}
+
+		if v == '?' {
+			sb.WriteString("'%")
+			sb.WriteByte(Type[flag])
+			sb.WriteString("'")
+			flag++
+		} else {
+			sb.WriteRune(v)
+
+		}
+	}
+	for i, v := range args {
+		args[i] = Mysql_real_escape_string(v)
+	}
+
+	fmt.Printf(sb.String(), args...)
+
+}

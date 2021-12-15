85a2e769ae15c8d1651ce7cf39211be931f0ccf4
Meteor <coopticks@outlook.com>
2021-12-15 15:07:43 +0800

    Add files via upload

---
+++ examples/test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	Queue "https://github.com/MeteorsLiu/MySQLQueue"
+	"fmt"
+	"sync"
+)
+
+
+func main() {
+	goCh := make(chan struct{})
+	Client := Queue.NewMySQLQueue("localhost", "3306", "test", "123456", "test", goCh)
+
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		sql, _ := Client.Query(fmt.Sprintf("SELECT text FROM test WHERE name='%s'", Queue.mysql_real_escape_string("TestES")))
+		for _, v := range sql {
+			fmt.Println(v["text"])
+		}
+	}()
+
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+		sql, _ := Client.Query(fmt.Sprintf("SELECT text FROM test WHERE name='%s'", Queue.mysql_real_escape_string("TestES")))
+		for _, v := range sql {
+			fmt.Println(v["text"])
+		}
+	}()
+
+	fmt.Println(Client.Query("SELECT * FROM test"))
+	wg.Wait()
+
+	//Shutdown Goroutine Worker
+	close(goCh)
+}

ec6ff3244f3a475daa57925b2be22a2e971df6ae
Meteor <coopticks@outlook.com>
2021-12-16 09:16:58 +0800

    Add AutoBindParam()

--- mysqlqueue.go
+++ mysqlqueue.go
@@ -60,6 +60,40 @@ func Mysql_real_escape_string(param string) string {
 }
 
 
+func Mysql_real_escape_byte(param []byte) string {
+	var sb strings.Builder
+	//Source: #789 escape_string_for_mysql https://github.com/mysql/mysql-server/blob/5.7/mysys/charset.c
+	for _, v := range param {
+		switch v {
+		case '\n':
+			sb.WriteByte('\\')
+			sb.WriteByte('n')
+		case '\r':
+			sb.WriteByte('\\')
+			sb.WriteByte('r')
+		case 0:
+			sb.WriteByte('\\')
+			sb.WriteByte('0')
+		case '\\':
+			sb.WriteByte('\\')
+			sb.WriteByte('\\')
+		case '\'':
+			sb.WriteByte('\\')
+			sb.WriteByte('\'')
+		case '"':
+			sb.WriteByte('\\')
+			sb.WriteByte('"')
+		case '\032':
+			sb.WriteByte('\\') /* This gives problems on Win32 */
+			sb.WriteByte('Z')
+		default:
+			sb.WriteByte(v)
+		}
+	}
+	return sb.String()
+}
+
+
 func NewMySQLQueue(addr, port, user, password, db string, ctx context.Context) SQLQueue {
 	in := make(chan string)
 	key := make(chan string)
--- mysqltranslate.go
+++ mysqltranslate.go
@@ -39,3 +39,53 @@ func BindParam(SQL, Type string, args ...interface{}) (string, error) {
 	return fmt.Sprintf(sb.String(), args...), nil
 
 }
+
+
+func AutoBindParam(SQL, args ...interface{}) (string, error) {
+	ArgsLength := len(args) - 1
+
+	for i, v := range SQL {
+		if v == '?' {
+			if flag <= ArgsLength {
+				sb.WriteString("'")
+				switch val := args[i].(type) {
+				case string:
+					sb.WriteString(Mysql_real_escape_string(val))
+				case int, int8, int16, int32, int64:
+					sb.WriteString(Mysql_real_escape_string(strconv.FormatInt(int64(val), 10))
+
+				case uint, uint8, uint16, uint32, uint64:
+					sb.WriteString(Mysql_real_escape_string(strconv.FormatUint(uint64(val), 10))
+				case float32:
+					sb.WriteString(Mysql_real_escape_string(strconv.FormatFloat(val, 'f', -1, 32))
+				case float64:
+					sb.WriteString(Mysql_real_escape_string(strconv.FormatFloat(val, 'f', -1, 64))
+				case Byte:
+					sb.WriteString(Mysql_real_escape_byte(val))
+				case Rune:
+					sb.WriteRune(val)
+
+				case Boolean:				
+					if val {
+   						sb.WriteString("1")
+					} else {
+   						sb.WriteString("0")
+					}
+
+				default:
+					return "", errors.New("Unknow Type")
+				}
+				sb.WriteString("'")
+
+			} else {
+				return "", errors.New("Out of indexs")
+			}
+			flag++
+		} else {
+			sb.WriteRune(v)
+		}
+	}
+
+	return sb.String(), nil
+
+}

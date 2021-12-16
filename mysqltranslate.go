6e76d2861ff24dd160f2d511ec0f66b4beefdd62
Meteor <coopticks@outlook.com>
2021-12-16 15:22:01 +0800

    Stupid but useful
    It can reduce your error

--- mysqltranslate.go
+++ mysqltranslate.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"fmt"
 	"strings"
-	"strconv"
 )
 
 func BindParam(SQL, Type string, args ...interface{}) (string, error) {
@@ -44,23 +43,36 @@ func BindParam(SQL, Type string, args ...interface{}) (string, error) {
 
 func AutoBindParam(SQL string, args ...interface{}) (string, error) {
 	ArgsLength := len(args) - 1
-
-	for i, v := range SQL {
+	flag := 0
+	var sb strings.Builder
+	for _, v := range SQL {
 		if v == '?' {
 			if flag <= ArgsLength {
 				sb.WriteString("'")
 				switch val := args[flag].(type) {
 				case string:
 					sb.WriteString(Mysql_real_escape_string(val))
-				case int, int8, int16, int32, int64:
-					sb.WriteString(Mysql_real_escape_string(strconv.FormatInt(int64(val), 10))
+				case int:
+					sb.WriteString(Mysql_real_escape_string(strconv.Itoa(val, 10)))
+				case int8:
+					sb.WriteString(Mysql_real_escape_string(strconv.FormatInt(int64(val), 10)))
+				case int16:
+					sb.WriteString(Mysql_real_escape_string(strconv.FormatInt(int64(val), 10)))
+				case int64:
+					sb.WriteString(Mysql_real_escape_string(strconv.FormatInt(val, 10)))
 
-				case uint, uint8, uint16, uint32, uint64:
-					sb.WriteString(Mysql_real_escape_string(strconv.FormatUint(uint64(val), 10))
+				case uint:
+					sb.WriteString(Mysql_real_escape_string(strconv.FormatUint(uint64(val), 10)))
+				case uint16:
+					sb.WriteString(Mysql_real_escape_string(strconv.FormatUint(uint64(val), 10)))
+				case uint32:
+					sb.WriteString(Mysql_real_escape_string(strconv.FormatUint(uint64(val), 10)))
+				case uint64:
+					sb.WriteString(Mysql_real_escape_string(strconv.FormatUint(val, 10)))
 				case float32:
-					sb.WriteString(Mysql_real_escape_string(strconv.FormatFloat(val, 'f', -1, 32))
+					sb.WriteString(Mysql_real_escape_string(strconv.FormatFloat(float64(val), 'f', -1, 32)))
 				case float64:
-					sb.WriteString(Mysql_real_escape_string(strconv.FormatFloat(val, 'f', -1, 64))
+					sb.WriteString(Mysql_real_escape_string(strconv.FormatFloat(val, 'f', -1, 64)))
 				case []byte:
 					sb.WriteString(Mysql_real_escape_bytes(val))
 				case byte:
